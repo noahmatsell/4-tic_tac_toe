560131e441d2fe7a915edbde6a89f8546d39315a
noahub <noah.matsell@unbounce.com>
2017-02-21 13:48:13 -0800

    Revert "fixed draw"
    This reverts commit 967caf5959204bc30ab29721280d4ace2573e1ca.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*-----------
-Vars
------------*/
+//load start screen
 var start = document.getElementById("start");
 var board = document.getElementById("board");
 var finish = document.getElementById("finish");
@@ -9,10 +7,19 @@ var startButton = document.getElementById("startButton");
 var oBox = document.getElementById("player1");
 var xBox = document.getElementById("player2");
 var newGame = document.getElementById("newGame");
-var onePlayerSelect = document.getElementById("one_player");
-var twoPlayerSelect = document.getElementById("two_player");
 
-//Game Object
+board.classList.add("hidden");
+finish.classList.add("hidden");
+//update xName
+xBox.insertAdjacentHTML('beforeend', "<div class='displayName'></div>");
+//update oName
+oBox.insertAdjacentHTML('beforeend', "<div class='displayName'></div>");
+
+//create player prototype
+//"E" stands for empty board cell
+
+//create game prototype
+//board state
 var game = { 
   board: ["E", "E", "E", "E", "E", "E", "E", "E", "E"],
   turn: "x",
@@ -29,6 +36,7 @@ var game = {
       switchViewTo("newGame");
     }
     else if(isFinished()) {
+        this.status = "ended";
         switchViewTo("end");
     }
     else {
@@ -45,44 +53,43 @@ var game = {
   }
 };
 
-/*----------
-Functions
-----------*/
+//current player has .active added to li
+//when current player hovers, add background-image to tile
+//on click, if tile empty, add class box-filled-1 (o) or box-filled-2 (x)
 
-//Check for end-game conditions
+//game end when 3 symbols in a row vertically, horizontally, or diagonally
 var isFinished = function(){        
   var B = game.board;
-  //draw
-  if (B[0] !== "E" && B[1] !== "E" && B[2] !== "E" && B[3] !== "E" && B[4] !== "E" && B[5] !== "E" && B[6] !== "E" && B[7] !== "E" && B[8] !== "E"){
-    game.status = "draw";
-    return true;
-  }else{
-    //check rows
-    for(var i = 0; i <= 6; i = i + 3) {
+  //check rows
+  for(var i = 0; i <= 6; i = i + 3) {
       if(B[i] !== "E" && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
-        game.status = "win";
-        return true;
+          game.status = "win";
+          return true;
       }
-    }
-    //check columns
-    for(var i = 0; i <= 2 ; i++) {
+  }
+  //check columns
+  for(var i = 0; i <= 2 ; i++) {
       if(B[i] !== "E" && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
-        game.status = "win";
-        return true;
+          game.status = "win";
+          return true;
       }
-    }
-    //check diagonals
-    for(var i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
+  }
+  //check diagonals
+  for(var i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
       if(B[i] !== "E" && B[i] == B[i + j] && B[i + j] === B[i + 2*j]) {
-        game.status = "win";
-        return true;
+          game.status = "win";
+          return true;
       }
-    }
+  }
+  //draw
+  if (B[0] !== "E" && B[1] !== "E" && B[2] !== "E" && B[3] !== "E" && B[4] !== "E" && B[5] !== "E"){
+      game.status = "draw";
+      return true;
   }
 };
-//UI change handler  
+  
 var switchViewTo = function(view){
-  //initialize new game
+  
   if (view === "newGame"){
     start.classList.add("hidden");
     finish.classList.add("hidden");
@@ -98,33 +105,26 @@ var switchViewTo = function(view){
     //remove old names
     xBox.querySelector(".displayName").textContent = game.xName;
     oBox.querySelector(".displayName").textContent = game.oName;
-  }
-  //start o turn
-  else if (view === "oTurn"){
+    
+  }else if (view === "oTurn"){
     console.log("o turn");
     xBox.classList.remove("active");
     oBox.classList.add("active");
-  }
-  //start x turn
-  else if (view === "xTurn"){
+  }else if (view === "xTurn"){
     console.log("x turn");
     oBox.classList.remove("active");
     xBox.classList.add("active");
-  }
-  //go to end state
-  else if (view === "end"){
+  }else if (view === "end"){
     start.classList.add("hidden");
     board.classList.add("hidden");
     finish.classList.remove("hidden");
-    //show draw
+
     if(game.status === "draw"){
       finish.classList.remove("screen-win-one");
       finish.classList.remove("screen-win-two");
       finish.classList.add("screen-win-tie");
       finish.querySelector("header .message").textContent = "It's a draw!";
-    }
-    //show winner
-    else{
+    }else{
       var winner;
       if (game.turn === "x"){
         winner = game.xName;
@@ -139,7 +139,13 @@ var switchViewTo = function(view){
       }
       finish.querySelector("header .message").textContent = game.turn + " wins! Congrats "+winner+"!";
     }
+
   }
+
+  //hide board
+  //show end screen - winner or tie
+  //screen-win-one for p1 screen-win-two for p2
+  //display winner name
 };
 
 var scoring = function(){
@@ -148,7 +154,9 @@ var scoring = function(){
   //else return 0
 }
 
-//Hover icon behaviour
+//clicking new game restarts game
+
+//
 var hover = function(){
   if (game.turn === "x" && this.className != 'box box-filled-2' && this.className != 'box box-filled-1') {
     this.classList.add('xSVG');
@@ -157,13 +165,14 @@ var hover = function(){
     this.classList.add('oSVG');
   }
 };
+
 var hoverOut = function(){
   if (this.className != 'box-filled-2' || this.className != 'box-filled-1') {
     this.classList.remove('xSVG');
     this.classList.remove('oSVG');
   }
 };
-//Game tile click behaviour
+
 var clickBox = function(){
   if (game.turn === "x") {
     if (this.className != 'box box-filled-2' && this.className != 'box box-filled-1') {
@@ -203,39 +212,17 @@ var clickBox = function(){
       }
   }
 };
-
-/*-------
-On Load
--------*/
-//Initial page
-board.classList.add("hidden");
-finish.classList.add("hidden");
-//update xName
-xBox.insertAdjacentHTML('beforeend', "<div class='displayName'></div>");
-//update oName
-oBox.insertAdjacentHTML('beforeend', "<div class='displayName'></div>");
-
 /*----------------
 Event Listeners
 ----------------*/
-//Start Button
 startButton.addEventListener("click", function(){
   game.advanceTo("start");
 });
-//Game type selection
-onePlayerSelect.addEventListener("click", function(){
-  twoPlayerSelect.parentElement.classList.remove("selected");
-  onePlayerSelect.parentElement.classList.add("selected");
-});
-twoPlayerSelect.addEventListener("click", function(){
-  onePlayerSelect.parentElement.classList.remove("selected");
-  twoPlayerSelect.parentElement.classList.add("selected");
-});
-//New Game
+
 newGame.addEventListener("click", function(){
   game.advanceTo("start");
 });
-//Game Tile boxes
+
 for (var i = 0; i < boardBoxes.length; i++) {
         var box = boardBoxes[i];
         box.addEventListener('mouseover', hover, false);
